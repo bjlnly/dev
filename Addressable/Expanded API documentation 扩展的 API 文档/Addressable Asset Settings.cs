659d5b5ec409ec168bbfbffabc243c73ce3d2b2e
bjlnly <117634430@qq.com>
2021-08-31 10:00:30 +0800

    [m]修改路径  增加Setting

---
+++ Addressable/Expanded API documentation 扩展的 API 文档/Addressable Asset Settings.cs
@@ -0,0 +1,14 @@
+/*
+    https://docs.unity3d.com/Packages/com.unity.addressables@1.18/manual/AddressableAssetSettings.html
+    Addressable Asset Settings 可寻址的资产设置
+        Manage Groups
+        Profile
+        Diagnostics 
+        Catalog 
+        Content Update
+        Downloads
+        Build
+        Build and Play Mode Scripts
+        Asset Group Templates
+        Initialization Objects
+ */
