5e7c14c8c9b41af205615731d5be327827a30c80
bjlnly <117634430@qq.com>
2021-09-01 11:46:34 +0800

    [a]增加核心api

---
+++ Addressable/Expanded API documentation 扩展的 API 文档/核心Api.cs
@@ -0,0 +1,15 @@
+/*
+    Expanded API documentation 扩展的 API 文档
+        AddressableAssetSettings        配置
+        BuildPlayerContent              build内容
+        DownloadDependenciesAsync       下载
+        ExceptionHandler                异常捕获
+        InitializeAsync                 初始化
+        InstantiateAsync                实例化
+        LoadContentCatalogAsync         加载Catalog
+        LoadingAddressableAssets        加载资源
+        LoadResourceLocationsAsync      加载Resources资源
+        LoadSceneAsync                  加载场景
+        TransformInternalId             修改Address内部ID,修改加载路径
+        UpdateCatalogs                  更新Catalog
+ */
